67d7d2fcb301d98ca13afea74b1e9f835df2bf2f
Damian Błaszczyk <60239406+damianblaszczyk@users.noreply.github.com>
2020-05-16 02:21:23 +0200

    Update ReCaptchaV2.pm

--- ReCaptchaV2.pm
+++ ReCaptchaV2.pm
@@ -19,37 +19,33 @@ sub new
 		_url		=> 'http://api.anti-captcha.com/',
 	};
 
+	for (keys % { $self })
+	{ $self->{$_} or croak "".$_." is required."; }
+
 	bless $self => $class;
 
 	return $self;
 }
 
-sub createtask 
+sub setopt
 {
 	my $self	= shift(@_);
+	my $opts 	= shift(@_);
 
-	my %sendit;
-	my %method;
+	for (keys % { $opts })
+	{$self->{opt}->{$_} = $opts->{$_};}
+}
+
+sub request
+{
+	my $self	= shift(@_);
+	my $method 	= shift(@_);
+	my $sendit 	= shift(@_);
 
 	my $json;
 	my $rcvit;
 
-	%method =
-	(
-		_create 	=> '/createTask',
-	);
-
-	%sendit =
-	(
-		clientKey	=> $self->{_apikey},
-		task 		=> {
-				type 			=> 'NoCaptchaTaskProxyless',
-				websiteURL		=> $self->{_domain},
-				websiteKey		=> $self->{_keysite},
-		},
-	);
-
-	$json = encode_json \%sendit;
+	$json = encode_json $sendit;
 
 	$self->{_browser} = WWW::Mechanize->new();
 	$self->{_browser}->add_header
@@ -59,13 +55,54 @@ sub createtask
 
 	$self->{_browser}->post
 	(
-		$self->{_url} . $method{_create}, 
+		$self->{_url} . $method, 
 		Content => $json
 	);
 
 	$rcvit = decode_json($self->{_browser}->response()->decoded_content());
 
-	$rcvit->{errorId} == 0 ? return $rcvit->{taskId} : return undef;
+	if ($method eq '/createTask')
+	{
+		$rcvit->{errorId} == 0 ? return $rcvit->{taskId} : return undef;
+	}
+	elsif ($method eq '/getTaskResult')
+	{
+		return $self->{_browser}->response()->decoded_content();
+	}
+}
+
+sub createtask 
+{
+	my $self	= shift(@_);
+	my $type;
+
+	my %sendit;
+
+	if (defined($self->{opt}->{proxyAddress}))
+	{
+		$type = 'NoCaptchaTask';
+	}
+	else
+	{
+		$type = 'NoCaptchaTaskProxyless';	
+	}
+
+	%sendit =
+	(
+		clientKey	=> $self->{_apikey},
+		task 		=> {
+				type 			=> $type,
+				websiteURL		=> $self->{_domain},
+				websiteKey		=> $self->{_keysite},
+		},
+	);
+
+	for (keys % { $self->{opt} })
+	{$sendit{task}{$_} = $self->{opt}->{$_};}
+
+	print Dumper(\%sendit); use Data::Dumper;
+
+	$self->request('/createTask', \%sendit);
 }
 
 sub checktask
@@ -74,15 +111,6 @@ sub checktask
 	my $task 	= shift(@_);
 
 	my %sendit;
-	my %method;
-
-	my $json;
-	my $rcvit;
-
-	%method =
-	(
-		_get 		=> '/getTaskResult',
-	);
 
 	%sendit =
 	(
@@ -90,21 +118,7 @@ sub checktask
 		taskId 		=> $task,
 	);
 
-	$json = encode_json \%sendit;
-
-	$self->{_browser} = WWW::Mechanize->new();
-	$self->{_browser}->add_header
-	( 
-		'content-type' => 'application/json',
-	);
-
-	$self->{_browser}->post
-	(
-		$self->{_url} . $method{_get}, 
-		Content => $json
-	);
-
-	return $self->{_browser}->response()->decoded_content();
+	$self->request('/getTaskResult', \%sendit);
 }
 
 sub waittask
@@ -143,35 +157,46 @@ ReCaptchaV2 - Auto solve captchas from Google v2
 
 http://api.anti-captcha.com/
 
+https://anticaptcha.atlassian.net/wiki/spaces/API/pages/5079089/NoCaptchaTask+Google+Recaptcha+puzzle+solving
+
 =head1 VERSION
 
 version 1.00
 
 =head1 SYNOPSIS
 
-	use ReCaptchaV2;
+use lib ".";
+use ReCaptchaV2;
 
-	my $captcha = new ReCaptchaV2
+	my $captcha;
+	my $res;
+	my $task;
+	my @tasks;
+
+	$captcha = new ReCaptchaV2
 	(
 		"apikey",
-		"https://domain.com",
-		"sitekey",
+		"domain.com",
+		"google-site-key",
 	);
 
-		$task = $captcha->createtask();
-	!$task ? warn "Problem with create task\r\n" : push(@tasks, $task);
+	$captcha->setopt({
+		'proxyType'		=> 'http',
+		'proxyAddress'	=> 'x.x.x.x',
+		'proxyPort'		=> 'y.y.y.y',
+		'userAgent'		=> 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36',
+		});
 
-	push(@tasks, $captcha->createtask());
-	push(@tasks, $captcha->createtask());
-	push(@tasks, $captcha->createtask());
-	push(@tasks, $captcha->createtask());
+	$task = $captcha->createtask();
+	print "$task\r\n";
 
-	foreach my $task (@tasks)
-	{
-		# Return output in JSON
-		$res = $captcha->checktask($task);
-		print "".$res."\r\n";	
-	}
+	sleep 150;
 
-	# Waiting to solved and return clean key
-	$captcha->waittask($task);
+	# Return JSON
+	$res = $captcha->checktask($task);
+	print "".$res."\r\n";
+
+	# waittask($task);
+	# Return when response, only google site code
+
+	return 0;
